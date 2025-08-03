b1c48e21c0bc94a300eb5a39aa58f3d769fddb55
unknown <randrianryfinaritra@gmail.com>
2025-08-03 12:55:17 +0200

    form

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
 console.log('Hello World');
+
+absolutePath = __dirname + '/views/index.html';
 app.get('/', myRoute);
 function myRoute (req, res) {
-    res.send('Hello Express');
+    // res.send('Hello Express');
+    res.sendFile(absolutePath);
 }
 
 
