603a764891f1367f44ccc0ffca7e27dee7d9d5ea
unknown <randrianryfinaritra@gmail.com>
2025-08-03 13:43:39 +0200

    form

--- myApp.js
+++ myApp.js
@@ -3,12 +3,16 @@ let app = express();
 console.log('Hello World');
 
 absolutePath = __dirname + '/views/index.html';
+
+
 app.get('/', myRoute);
 function myRoute (req, res) {
     // res.send('Hello Express');
     res.sendFile(absolutePath);
 }
 
+stylePth = __dirname + '/public';
+app.use('/public', express.static(stylePth));
 
 
 
