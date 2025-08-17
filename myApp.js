bea189007e4c539716b130d9d7e916ce94b8c2eb
unknown <randrianryfinaritra@gmail.com>
2025-08-17 22:52:13 +0200

    json

--- myApp.js
+++ myApp.js
@@ -6,11 +6,11 @@ require('dotenv').config()
 absolutePath = __dirname + '/views/index.html';
 
 
-app.get('/', myRoute);
-function myRoute (req, res) {
-    // res.send('Hello Express');
-    res.sendFile(absolutePath);
-}
+// app.get('/', myRoute);
+// function myRoute (req, res) {
+//     // res.send('Hello Express');
+//     res.sendFile(absolutePath);
+// }
 
 stylePth = __dirname + '/public';
 app.use(express.static(stylePth));
@@ -18,13 +18,14 @@ app.use(express.static(stylePth));
 const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
 
 app.get('/json', (req, res) => {
-    let message = 'Hello json'
-    if (MESSAGE_STYLE ==='uppercase'){
-        message = message.toUpperCase()
-    }
-    res.json({"message" : message})
-})
-
+  let response = { message: "Hello json" };
+  
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response.message = response.message.toUpperCase();
+  }
+  
+  res.json(response);
+});
 
 
 
