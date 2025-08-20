961aaf6fc92d85aa6c393449e334ef93e5f29da0
unknown <randrianryfinaritra@gmail.com>
2025-08-20 14:23:17 +0200

    json

--- myApp.js
+++ myApp.js
@@ -5,18 +5,25 @@ var bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.route('/name')
-.get((req, res) => {
-  const first = req.query.first
-  const last = req.query.last
+// app.route('/name')
+// .get((req, res) => {
+//   const first = req.query.first
+//   const last = req.query.last
 
-res.json({name :`${first} ${last}`})
-})
-.post((req, res) => {
- const first = req.query.first
-  const last = req.query.last
+// res.json({name :`${first} ${last}`})
+// })
+// .post((req, res) => {
+//  const first = req.query.first
+//   const last = req.query.last
 
-res.json({name :`${first} ${last}`})
+// res.json({name :`${first} ${last}`})
+// })
+app.post('/name',(req, res) => {
+  // var {first : firstname, last:lastname} = req.body
+ const first = req.body.first
+  const last = req.body.last
+
+res.json({name :`${firstname} ${lastname}`})
 })
 // app.get('/:word/echo', (req, res) =>{
 //   const {word} = req.params;
@@ -47,14 +54,14 @@ res.json({name :`${first} ${last}`})
 //   console.log(req.method, req.path, req.ip)
 //   next();
 // }
-// absolutePath = __dirname + '/views/index.html';
+absolutePath = __dirname + '/views/index.html';
 
 
-// app.get('/', myRoute);
-// function myRoute (req, res) {
-//     // res.send('Hello Express');
-//     res.sendFile(absolutePath);
-// }
+app.get('/', myRoute);
+function myRoute (req, res) {
+    // res.send('Hello Express');
+    res.sendFile(absolutePath);
+}
 
 // app.use("/public", express.static(__dirname + "/public"));
 
